d3db59e9afe7fc51954d4e4946f55273a6981074
lenguyentrinh <lethinguyentrinh.work@gmail.com>
2026-04-03 22:22:50 +0700

    update UI to support responsive design on all screen

--- app/(auth)/forgotPassword/otp/page.tsx
+++ app/(auth)/forgotPassword/otp/page.tsx
@@ -2,8 +2,8 @@ import OtpForm from "./otpForm";
 
 export default function otp() {
   return (
-    <div className="bg-login flex min-h-screen items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-md w-full md:w-2/7 ">
+    <div className="bg-login flex min-h-screen items-center justify-center p-4 sm:p-6">
+      <div className="bg-white p-5 sm:p-8 rounded shadow-md w-full max-w-md">
               <h1 className="font-semibold  text-center mb-3">Enter Verification Code</h1>
               < OtpForm/>
             </div>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -2,9 +2,9 @@ import SignupForm from "./SignupForm";
 
 export default function SignupPage() {
   return (
-    <div className="bg-login flex min-h-screen items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-md w-full md:w-2/7 ">
-        <h1 className="font-semibold text-4xl text-center mb-3">Wellcome</h1>
+    <div className="bg-login flex min-h-screen items-center justify-center p-4 sm:p-6">
+      <div className="bg-white p-5 sm:p-8 rounded shadow-md w-full max-w-2xl">
+        <h1 className="font-semibold text-3xl sm:text-4xl text-center mb-3">Wellcome</h1>
         <SignupForm/>
         <div className="flex text-center justify-center text-gray-500 hover:text-gray-700 pt-4">Have your an account? 
             <a href="/login" className="underline">Login</a>
